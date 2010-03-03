38bd814c20700c1eef10619b5ba470c0e2f81f0e
Timothy Pratley <timothypratley@gmail.com>
2010-03-03 22:54:31 +1100

    Local server running

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject kudosweave "0.1.0-SNAPSHOT"
             :description "Kudos Weave"
-            :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                           [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
+            :repositories [["mvnsearch" "http://www.mvnsearch.org/maven2/"]]
+            :dependencies [[org.clojure/clojure "1.1.0"]
+                           [org.clojure/clojure-contrib "1.1.0"]
                            [compojure "0.3.2"]
+                           [com.google.appengine/appengine-tools-sdk "1.3.1"]
                            [tjpext "1.0.0-SNAPSHOT"]]
-            :dev-dependencies [[leiningen-run "0.2"]])
+            :main com.kudosweave.server
+            :dev-dependencies [[leiningen-run "0.2"]
+                               [uk.org.alienscience/leiningen-war "0.0.1"]])
