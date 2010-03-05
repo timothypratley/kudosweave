21c4b80716e69fc2c7f06a9f7b9dcd5ea7aca87e
Timothy Pratley <timothypratley@gmail.com>
2010-03-05 23:17:05 +1100

    layout updated

--- src/com/kudosweave/servlet.clj
+++ src/com/kudosweave/servlet.clj
@@ -1,25 +1,21 @@
 (ns com.kudosweave.servlet
   (:gen-class :extends javax.servlet.http.HttpServlet)
   (:use com.kudosweave.template)
+  (:use com.kudosweave.home)
+  (:use com.kudosweave.attack)
+  (:use com.kudosweave.ally)
   (:use ring.util.servlet)
   (:use compojure))
 
-(defn success [body]
-  {:status 200 :headers {} :body body})
-(defn failure [body]
-  {:status 404 :headers {} :body body})
 
 (defroutes webservice
-           (GET "/" req (success
-                          (html-doc "Welcome"
-                                    [:p "This site will be a proof of concept network of trust."])))
-           (GET "/blah" req (success
-                              (html-doc "Blah"
-                                        [:p "Right now there is not much to see"])))
-           ;(GET "/*" req (or (serve-file (params :*)) :next))
-           (ANY "*" req (failure
-                          (html-doc "Not Found"
-                                    [:p "404 not found"]))))
+           (GET "/" req (home req))
+           (GET "/attack" req (attack req))
+           (GET "/ally" req (ally req))
+           (ANY "*" req {:status 404
+                         :headers {}
+                         :body (layout "Not Found"
+                                    [:p "404 not found"])}))
 
 (defservice webservice)
 
--- src/com/kudosweave/template.clj
+++ src/com/kudosweave/template.clj
@@ -2,26 +2,40 @@
   (:import [com.google.appengine.api.users UserServiceFactory])
   (:use hiccup))
 
-(defn html-doc
-  [title & body]
+(defn layout
+  [title content]
   (html [:html
          [:head
           [:title (str title " - Kudos Weave")]
           [:link {:rel "shortcut icon"
                   :href "/favicon.ico"
-                  :type="image/ico"}]]
+                  :type="image/ico"}]
+          [:link {:rel "stylesheet"
+                  :media "screen,projection"
+                  :type "text/css"
+                  :href "/css/main.css"}]
+          [:link {:rel "stylesheet"
+                  :media "print"
+                  :type "text/css"
+                  :href "/css/print.css"}]
+          [:link {:rel "stylesheet"
+                  :media "aural"
+                  :type "text/css"
+                  :href "/css/aural.css"}]]
          [:body
-          [:div
-           [:h2
-            [:a {:href "/"} "Kudos Weave"]]]
-            (let [user-service (UserServiceFactory/getUserService)]
+          [:div {:id "main", :class "box"}
+           [:div {:id "header"}
+            [:h1 {:id "logo"} [:a {:href "/"} "Kudos Weave"]]
+            [:p (let [user-service (UserServiceFactory/getUserService)]
               (if-let [user (.getCurrentUser (UserServiceFactory/getUserService))]
                 [:p (.getNickname user) " - "
                  [:a {:href (.createLogoutURL user-service "/")} "sign out"]]
-                [:a {:href (.createLoginURL user-service "/")} "sign in"]))
-          body
-          [:a {:href "http://code.google.com/appengine/"}
-           [:img {:src "http://code.google.com/appengine/images/appengine-silver-120x30.gif"
-                  :alt "Powered by Google App Engine"
-                  :title "Powered by Google App Engine"}]]]]))
+                [:h2 [:a {:href (.createLoginURL user-service "/")} "sign in"]]))]]
+           [:div {:id "content"} [:p content]]
+           [:div {:id "footer"}
+            [:center
+             [:a {:href "http://code.google.com/appengine/"}
+              [:img {:src "http://code.google.com/appengine/images/appengine-silver-120x30.gif"
+                     :alt "Powered by Google App Engine"
+                     :title "Powered by Google App Engine"}]]]]]]]))
 
